d2f3afb2fd96729c5fdb15287c7447be149b262d
rparker24 <robert@robertmparker.net>
2016-03-06 18:08:44 -0500

    add first question and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,3 @@
-
-// not sure where this should run yet or if it is needed
-$(document).ready(function() {
-
-})
-
-
-
 // timer object for keeping time
 var timer = {
   time:30,
@@ -19,20 +11,26 @@ var timer = {
     timer.time--
     $('#countdown').html(timer.time)
     if (timer.time === 0){
-      stop();
-      alert('Time Up!');
+      timer.stop();
+      alert('Time\'s Up!');
     }
   }
 }
-// make timer run on q1 div load
-// timer.run();
+var $timerDisplay = $('<p id="timeRemaining">' + "Time Remaining: " + '<span id="countdown">' + '</span>' + " Seconds" + '</p>');
+var $q1 = $('<div class="questions">' + '<p id="question1">' + '1. What material are hockey pucks made of?' + '</p>' + '<br>' + '<p class="answer incorrect">' + 'Plastic' + '</p>' + '<p class="answer correct">' + 'Rubber' + '</p>' + '<p class="answer incorrect">' + 'Wood' + '</p>' + '<p class="answer incorrect">' + 'Charcoal' + '</p>');
+var $q2 = $('<div class="questions">' + '<p id="question1">' + '2. What is it called when a player scores 3 goals in 1 game?' + '</p>' + '<br>' + '<p class="answer incorrect">' + 'Hole in One' + '</p>' + '<p class="answer incorrect">' + 'Swish' + '</p>' + '<p class="answer incorrect">' + 'Grand Slam' + '</p>' + '<p class="answer correct">' + 'Hat Trick' + '</p>');
 
-var $div = $('<div>' + timer.time + '</div>');
-var $q1 = $('<p>' + "Time Remaining: " + timer.time + '</p>');
-
-
+// when start button is clicked
 $('#button').click(function() {
-  $('#content').html($div, $q1);
+  $('#content').html($timerDisplay);
+  $('#content').append($q1);
   timer.time = 30;
   timer.run();
-})
+});
+
+$('.incorrect').click(function() {
+  $('#content').html($timerDisplay);
+  $('#content').append($q2);
+  timer.time = 30;
+  timer.run();
+});
